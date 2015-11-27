323a76ea42f280fd8776b9da46372fbb0f3edf52
Reynir15 <reynir15@ru.is>
2015-11-27 15:56:44 +0000

    switch cases

--- csdb/main.cpp
+++ csdb/main.cpp
@@ -7,24 +7,55 @@
 
 using namespace std;
 
+string commands[] = {"find","add","sort","display"};
+
 //prentar ut upplysingar yfir commands
 void printHelp(){
 
 }
 //les skipun fra notenda og skilar nr yfir hvada skipun eigi ad framkvaema
 int readCommand(string command){
+    int cnumber = -1;
+    for(unsigned int i = 0; i < (sizeof(commands)/sizeof(*commands)); i++){
+        if(commands[i] == command)
+            cnumber = i;
+    };
 
+    switch(cnumber)
+    {
+        //find
+        case 0:
+            //statement
+            break;
+        //add
+        case 1:
+            //statement;
+            break;
+        //sort
+        case 2:
+            //statement;
+            break;
+        //display
+        case 3:
+            //statement
+            break;
+        default:
+            cout << "teo";
+            //statement;
+
+    }
     return -1;
+
 }
 
-int main(int argc, char *argv[])
+int main()
 {
 
-    QCoreApplication a(argc, argv);
+
     person newPerson;
     cin >> newPerson;
 
     cout << newPerson;
-    return a.exec();
+    return 0;
 }
 
